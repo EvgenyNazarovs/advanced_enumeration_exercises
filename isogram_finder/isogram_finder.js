831c588d3fe0071590cda9959142feaf0b3a6539
Evgeny Nazarovs <eugene.nazarovs@gmail.com>
2020-04-15 23:43:51 +0100

    refactors isograms

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -5,16 +5,12 @@ const IsogramFinder = function (word) {
 IsogramFinder.prototype.isIsogram = function () {
   let arr = this.word.split('')
   const overallTally = arr.reduce((tally, letter) => {
-    if (tally[letter]) {
-      tally[letter]++
-    } else {
-    tally[letter] = 1
-  } return tally
-}, {})
+    if (tally[letter]) tally[letter] += 1;
+    else tally[letter] = 1;
+    return tally
+  }, {})
 
-  const result = Object.values(overallTally).every((n) => (
-      n <= 1)
-  )
+  const result = Object.values(overallTally).every(n => n <= 1)
 
   return result;
 }
